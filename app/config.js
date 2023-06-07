d69d62302bddecdb8a773756ecff96f0187b821e
Emma <ejames@companieshouse.gov.uk>
2023-06-07 11:35:20 +0100

    Adding CHS redesign screens to Marks prototype

--- app/config.js
+++ app/config.js
@@ -4,10 +4,10 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Find and update company information',
+  serviceName: 'Find and update company information prototype',
 
   // Default port that prototype runs on
-  port: '3000',
+  port: '3008',
 
   // Enable or disable password protection on production
   useAuth: 'true',
