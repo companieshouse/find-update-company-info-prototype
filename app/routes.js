e3ce791e922a0effb49d68d9a18dd563928a0361
Emma <ejames@companieshouse.gov.uk>
2023-07-25 10:06:18 +0100

    Updated to Latest version of the prototype

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const router = express.Router()
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
 
 // Add your routes here - above the module.exports line
 
 router.use('/', require('./routes/v1-routes.js'))
 
 router.use('/', require('./routes/v2-routes.js'))
-
-
-module.exports = router
-
-
-
-
