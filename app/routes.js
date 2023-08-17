be5641a97aa08f7aa289fb68fba0ebc8c7aca524
Emma <ejames@companieshouse.gov.uk>
2023-08-17 19:52:02 +0100

    Update to new version of prototype kit

--- app/routes.js
+++ app/routes.js
@@ -8,8 +8,8 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-// Add your routes here - above the module.exports line
 
 router.use('/', require('./routes/v1-routes.js'))
 
 router.use('/', require('./routes/v2-routes.js'))
+
